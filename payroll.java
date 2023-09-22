34b85cc8145757807cf6be762b17d28bbdf67a6f
@jojey <132944770+j-ojey@users.noreply.github.com>
2023-09-22 11:40:51 +0300

    Add files via upload
    Payroll code

---
+++ payroll.java
@@ -0,0 +1,13 @@
+package payrollcomputation;
+import java.util.Scanner;
+public class payroll {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String[] employee = {"employeenumber", "employee name", "designation", "days worked", "generation date"};
+		String[] cash = {"payrate"};
+		
+
+	}
+
+}
